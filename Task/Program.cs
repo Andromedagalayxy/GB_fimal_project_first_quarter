fd9d397e250737bc3cc23050ff9b27e72a1e0f1e
Andromedagalayxy <Andromedagalayxy@gmail.com>
2023-03-25 19:59:38 +0500

    Task change 1

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class program 
+{
+  static void Main(string [] args) 
+  {
+      Console.WriteLine("How many array cells do you want to create?");
+      
+      string input;
+      int cells_count, cell_lenght, cell_count, count;
+      string [] myarray;
+      string [] taskarray;
+      
+      input = Console.ReadLine();
+      cells_count = Convert.ToInt32(input);
+      
+      myarray = new string [cells_count];
+      
+      
+      for (int i = 0; i<cells_count; i++)
+      {
+          Console.WriteLine("Enter cell name");
+          input = Console.ReadLine();
+          myarray[i] = input;
+      }
+      
+      for (int i = 0; i<cells_count; i++)
+      {
+          Console.WriteLine($"Element myarray [{i}] = {myarray[i]}");
+      }
+      
+      cell_count = 0;
+      
+      for (int i = 0; i<cells_count; i++)
+      {
+          cell_lenght = myarray[i].Length;
+          if (cell_lenght < 4) 
+          {
+              cell_count++;
+          }
+      } 
+      
+      taskarray = new string [cell_count];
+      count = 0;
+      
+      for (int i = 0; i<cells_count; i++)
+      {
+          if (count < cell_count)
+          {
+              cell_lenght = myarray[i].Length;
+              
+              if (cell_lenght < 4) 
+              {
+                  taskarray[i] = myarray[i];
+                  count++;
+              }
+          }
+          
+      }
+      
+      for (int i = 0; i<cell_count; i++)
+      {
+          Console.WriteLine($"Element task array [{i}] = {taskarray[i]}");
+      }
+  }
+}
